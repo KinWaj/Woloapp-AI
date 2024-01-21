735dea4e873eff936426aa69ebafa5a4ceeea649
Agata Dobrzyniewicz <91957042+ayakiri@users.noreply.github.com>
2024-01-21 13:32:41 +0100

    Wolo ai 05 recieving json and transformation based on language (#2)
    * WOLO-AI-05-recieving json and modifying it based on language
    
    WOLO-AI-05-recieving json and modifying it based on language
    
    * WOLO-AI-05-pylint fixes
    
    WOLO-AI-05-pylint fixes
    
    * Update pylint to install requierments
    
    * WOLO-AI-05 more errors exceptions
    
    WOLO-AI-05 more errors exceptions
    
    * WOLO-AI-05 deleted brad exception
    
    WOLO-AI-05 deleted brad exception
    
    * Remove .idea from repo
    
    ---------
    
    Co-authored-by: Kinga <Kinga.Wajda@protonmail.com>

---
+++ app.py
@@ -0,0 +1,57 @@
+"""Module for AI funtionalities"""
+
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+def translate_json(json_data):
+    """Translates recieved json data with AI and returns modified dictionary.
+     Creates keys for every language separately"""
+    try:
+        data_dict = dict(json_data)
+
+        languages = ['pl', 'eng', 'ru', 'uk']
+
+        for key in ['title', 'description', 'address_description']:
+            for lang in languages:
+                lang_key = f'{key}_{lang}'
+                if data_dict['language'] == lang:
+                    data_dict[lang_key] = f'{data_dict[key]}_original'
+                else:
+                    # AI translations goes here
+                    data_dict[lang_key] = f'{data_dict[key]}_{lang}_modified'
+
+        if 'language' in data_dict:
+            del data_dict['language']
+        if 'title' in data_dict:
+            del data_dict['title']
+        if 'description' in data_dict:
+            del data_dict['description']
+        if 'address_description' in data_dict:
+            del data_dict['address_description']
+
+        return data_dict
+
+    except KeyError as key_error:
+        return {'error': f'Missing key: {str(key_error)}'}
+    except ValueError as value_error:
+        return {'error': f'Invalid JSON data: {str(value_error)}'}
+
+
+@app.route('/translate', methods=['POST'])
+def translate_json_receiver():
+    """Receives json to translate and gives back translated version (also json)"""
+    try:
+        received_json = request.json
+
+        translated_data_dict = translate_json(received_json)
+
+        return jsonify(translated_data_dict)
+
+    except ValueError as value_error:
+        return jsonify({'error': f'Invalid JSON data: {str(value_error)}'})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
