31e97d41db6e0207a732b1b52ca432798ec83b7b
Agata Dobrzyniewicz <s24305@pjwstk.edu.pl>
2024-02-02 13:15:44 +0100

    WOLO-150-changed returning format to name and id, categories are hardcoded
    WOLO-150-changed returning format to name and id, categories are hardcoded

--- category_suggester_app.py
+++ category_suggester_app.py
@@ -5,15 +5,29 @@ from setfit import SetFitModel
 
 app = Flask(__name__)
 
+category_with_ids = {
+    "Kultura": 1,
+    "Sport": 2,
+    "Edukacja": 3,
+    "Ekologia": 4,
+    "Pomoc": 5
+}
+
 
 def suggest_category(json_data):
+    """Uses woloapp own model to suggest category based on description, return category name and id"""
     data_dict = dict(json_data)
     description = data_dict["description"]
 
     model = SetFitModel.from_pretrained("ayakiri/wolo-app-categories-setfit-model")
     category_suggested = model(description)
 
-    return category_suggested
+    category_suggested_dict = {
+        "name": category_suggested,
+        "id": category_with_ids[category_suggested]
+    }
+
+    return category_suggested_dict
 
 
 @app.route('/suggest', methods=['POST'])
