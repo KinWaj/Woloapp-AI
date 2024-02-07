fdcc2869bc463a6f6248578616276bed412e60f6
Agata Dobrzyniewicz <s24305@pjwstk.edu.pl>
2024-02-07 11:18:10 +0100

    Prepare binary matrix for user and all
    Prepare binary matrix for user and all

--- recommendation_app.py
+++ recommendation_app.py
@@ -15,44 +15,168 @@ class RecommendationHandler:
         self.mlb = MultiLabelBinarizer()
 
     def recommend(self, json_data):
+        """Recommends upcoming events based on user events. Needs to recieve json_data with user id"""
         data_dict = json_data
         user_id = data_dict["user_id"]
 
         user_events = list(self.get_users_events(user_id))
-
         upcoming_events = list(self.get_upcoming_events())
 
-        all_events = user_events + upcoming_events
+        user_matrix = self.prepare_matrix()
+        user_matrix = self.insert_events(user_events, user_matrix)
 
-        events_df = pd.DataFrame(all_events)
+        all_matrix = self.prepare_matrix()
+        all_matrix = self.insert_events(user_events, all_matrix)
+        all_matrix = self.insert_events(upcoming_events, all_matrix)
 
-        categories_matrix = self.mlb.fit_transform(events_df['categories'])
-        districts_matrix = self.mlb.fit_transform(events_df['district'])
-        organizations_matrix = self.mlb.fit_transform(events_df['organisation'])
+        # printing for debugging proces, to be deleted
+        pd.set_option('display.max_columns', None)
+        pd.set_option('display.expand_frame_repr', False)
+        print("USER")
+        print(user_matrix)
+        print("ALL")
+        print(all_matrix)
 
-        combined_matrix = pd.concat([pd.DataFrame(categories_matrix),
-                                     pd.DataFrame(districts_matrix),
-                                     pd.DataFrame(organizations_matrix)], axis=1)
+        #TODO - use binary matrixes to create model and predict
 
-        self.model.fit(combined_matrix)
-
-        indices = self.model.kneighbors([self.mlb.transform(user_events).sum(axis=0)], return_distance=False)
+        # self.model.fit(combined_matrix)
+        #
+        # indices = self.model.kneighbors([self.mlb.transform(user_combined_matrix).sum(axis=0)], return_distance=False)
+        #
+        # print(indices)
 
         return upcoming_events
 
     def get_users_events(self, user_id):
-        url = self.user_events_api_url.format(user_id)
-        response = requests.get(url)
+        """Calls API and return a list of events that user participated in"""
+        # url = self.user_events_api_url.format(user_id)
+        # response = requests.get(url)
+        #
+        # if response.status_code == 200:
+        #     return response.json()
+        # else:
+        #     raise Exception(f'API 1 request failed with status code {response.status_code}')
 
-        if response.status_code == 200:
-            return response.json()
-        else:
-            raise Exception(f'API 1 request failed with status code {response.status_code}')
+        json_dummy = [
+                  {
+                    "id": 9,
+                    "organisation": "PJATK",
+                    "district": "Orunia",
+                    "categories": ["Sport"]
+                  },
+                  {
+                    "id": 10,
+                    "organisation": "PJATK",
+                    "district": "Centrum",
+                    "categories": ["Sport"]
+                  },
+                  {
+                    "id": 11,
+                    "organisation": "Kwiatuszek",
+                    "district": "Wrzeszcz",
+                    "categories": ["Ekologia", "Pomoc"]
+                  }
+                ]
+
+        return json_dummy
+
 
     def get_upcoming_events(self):
-        response = requests.get(self.upcoming_events_api_url)
+        """Calls API and return a list of upcoming events"""
+        # response = requests.get(self.upcoming_events_api_url)
+        #
+        # if response.status_code == 200:
+        #     return response.json()
+        # else:
+        #     raise Exception(f'API 1 request failed with status code {response.status_code}')
 
-        if response.status_code == 200:
-            return response.json()
-        else:
-            raise Exception(f'API 1 request failed with status code {response.status_code}')
+        json_dummy = [
+                      {
+                        "id": 1,
+                        "organisation": "Kwiatuszek",
+                        "district": "Wrzeszcz",
+                        "categories": ["Sport"]
+                      },
+                      {
+                        "id": 2,
+                        "organisation": "Bananek",
+                        "district": "Przymorze",
+                        "categories": ["Edukacja"]
+                      },
+                      {
+                        "id": 3,
+                        "organisation": "PJATK",
+                        "district": "Orunia",
+                        "categories": ["Sport", "Pomoc"]
+                      },
+                      {
+                        "id": 4,
+                        "organisation": "PJATK",
+                        "district": "Centrum",
+                        "categories": ["Sport"]
+                      },
+                      {
+                        "id": 5,
+                        "organisation": "Bananek",
+                        "district": "Wrzeszcz",
+                        "categories": ["Kultura"]
+                      },
+                      {
+                        "id": 6,
+                        "organisation": "Kwiatuszek",
+                        "district": "Wrzeszcz",
+                        "categories": ["Ekologia", "Pomoc"]
+                      },
+                      {
+                        "id": 7,
+                        "organisation": "Pieski i kotki",
+                        "district": "Orunia",
+                        "categories": ["Ekologia", "Pomoc"]
+                      },
+                      {
+                        "id": 8,
+                        "organisation": "Bananek",
+                        "district": "Przymorze",
+                        "categories": ["Kultura"]
+                      }
+                    ]
+
+        return json_dummy
+
+    def prepare_matrix(self):
+        """Prepare an empty matrix with all columns from API - categories, districts and organisations"""
+        # TODO - Import from API
+        categories = {
+            "categories": ["Kultura", "Sport", "Edukacja", "Ekologia", "Pomoc", "Podstawowa"]
+        }
+        districts = {
+            "districts": ["Wrzeszcz", "Centrum", "Przymorze", "Chelm", "Zaspa", "Oliwa", "Orunia"]
+        }
+        organisations = {
+            "organisations": ["Wesoly Senior", "Przedszkole Tecza", "ChessMaster", "Fundacja Druga Szansa", "PJATK", "Kwiatuszek", "Bananek", "Pieski i kotki"]
+        }
+
+        all_columns = [categories["categories"], districts["districts"], organisations["organisations"]]
+        all_columns = [item for sublist in all_columns for item in sublist]
+
+        df = pd.DataFrame(all_columns).transpose()
+        df.columns = df.iloc[0]
+        df = df[1:]
+
+        return df
+
+    def insert_events(self, events_list, df):
+        """Goes through list of events and inserts a rows to binary matrix"""
+        for event in events_list:
+            row_data = [0] * len(df.columns)
+
+            for category in event.get('categories', []):
+                row_data[df.columns.get_loc(category)] = 1
+
+            row_data[df.columns.get_loc(event['district'])] = 1
+            row_data[df.columns.get_loc(event['organisation'])] = 1
+
+            df.loc[len(df)] = row_data
+
+        return df
+
